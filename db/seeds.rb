1c1fef22ce88cee668794bdb5b76cfe9d9ff0991
nolanborz <nolanrborzoni@gmail.com>
2025-04-23 15:52:22 +0900

    Help pretty the views up some more

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+
+Product.create!(title: 'Grenade',
+  description:
+    %(<p>
+      <em>A tool for any job.</em>
+      A grenade is a handheld bomb thrown by hand or launched mechanically.</p>),
+      image_url: 'grenade.jpg',
+      price: 4.99)
+Product.create!(title: 'Rifle',
+  description:
+  %(<p>
+    <em>A small</em> bomb thrown by hand or launched mechanically.</p>),
+    image_url: 'rifle.jpg',
+    price: 4.99)
+Product.create!(title: 'Sniper',
+  description:
+  %(<p>
+    <em>A small</em> bomb thrown by hand or launched mechanically.</p>),
+    image_url: 'pistol.jpg',
+    price: 4.99)
+Product.create!(title: 'Pistol',
+  description:
+  %(<p>
+    <em>A small</em> bomb thrown by hand or launched mechanically.</p>),
+    image_url: 'sniper.jpg',
+    price: 4.99)
